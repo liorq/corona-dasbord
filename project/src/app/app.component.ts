ee4603b67234ffb4f26846718f10bf6d289d7436
lior daniel <liorqm@gmail.com>
2023-03-09 22:36:44 +0200

    rgrg

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -24,38 +24,54 @@ export class AppComponent implements AfterViewInit {
   @HostListener('window:scroll', ['$event'])
   onWindowScroll(event: any) {
     this.currentPosition = window.scrollY;
-    const num = this.currentPosition;
 
-    const ranges: any = [[0, 400, "מבט על"],
-    [401, 1501, "מדדים מרכזיים"],
-    [1502, 2322, "מדדי תחלואה כללית"],
-    [2322, 4257, "תחלואה ואשפוזי ילדים"],
-    [4258, 5788, "תחלואה מחול"],
-    [5789, 6988, "חולים קשה ומאושפזים"],
-    [6989, 7772, "נפטרים"],
-    [7775, 9372, "בדיקות"],
-    [9372, 10881, "תחקורים נוספים"],
-    [10881, 12072, "תחלואה חוזרת ומחלימים"],
-    [12072, 14033, "התחסנות האוכלוסיה"],
-    [14033, 15233, "רמזור בישובים"]
+    const ranges: any = [
+      [0, 400, 'מבט על'],
+      [401, 1501, 'מדדים מרכזיים'],
+      [1502, 2322, 'מדדי תחלואה כללית'],
+      [2322, 4257, 'תחלואה ואשפוזי ילדים'],
+      [4258, 5788, 'תחלואה מחול'],
+      [5789, 6988, 'חולים קשה ומאושפזים'],
+      [6989, 7772, 'נפטרים'],
+      [7775, 9372, 'בדיקות'],
+      [9372, 10881, 'תחקורים נוספים'],
+      [10881, 12072, 'תחלואה חוזרת ומחלימים'],
+      [12072, 14033, 'התחסנות האוכלוסיה'],
+      [14033, 15233, 'רמזור בישובים']
     ];
 
     for (const range of ranges) {
-      if (range == null) continue
+      if (range == null) continue;
 
-      if (num >= range[0] && num < range[1]) {
-
-        this.coronaSvc.componentNameActive.next(range[2])
-        this.scrollToDiv(range[2])
+      if (this.currentPosition >= range[0] && this.currentPosition < range[1]) {
+        this.coronaSvc.componentNameActive.next(range[2]);
+        this.scrollToDiv(range[2]);
         break;
       }
     }
-
   }
+
+  @HostListener('window:wheel', ['$event'])
+  onWindowWheel(event: any) {
+    const sections: any = document.querySelectorAll('section');
+
+    for (const section of sections) {
+      const rect = section.getBoundingClientRect();
+      const scrollY = window.scrollY || window.pageYOffset;
+      const diff = scrollY + window.innerHeight - rect.bottom;
+
+      // Check if the bottom of the section is within 200 pixels of the bottom of the screen
+      if (diff > -200 && diff <50) {
+        this.coronaSvc.componentNameActive.next(section.getAttribute('data-name'));
+        this.scrollToDiv(section.getAttribute('data-name'));
+        break;
+      }
+    }
+  }
+
   scrollToDiv(divName: string) {
-
-    var div = document.getElementById(divName);
-    if (div)
-      div?.scrollIntoView({ behavior: 'smooth' });
+    const div = document.getElementById(divName);
+    if (div) div?.scrollIntoView({ behavior: 'smooth' });
   }
+
 }
