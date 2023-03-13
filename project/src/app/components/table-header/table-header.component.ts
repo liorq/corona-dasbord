431543502311c8f565d61ebbc0199da241e54563
lior daniel <liorqm@gmail.com>
2023-03-13 21:08:16 +0200

    rgrg

--- project/src/app/components/table-header/table-header.component.ts
+++ project/src/app/components/table-header/table-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { clickCounterObj } from 'src/app/utils/app.arrays';
 
 @Component({
@@ -6,12 +6,15 @@ import { clickCounterObj } from 'src/app/utils/app.arrays';
   templateUrl: './table-header.component.html',
   styleUrls: ['./table-header.component.css','../generic-table/generic-table.component.css']
 })
-export class TableHeaderComponent {
-@Input() graphName?:string;
+export class TableHeaderComponent implements OnInit{
+  @Input() graphName?:string;
 @Input() tablesData?:any=[];
 @Input()isDarkModeActive?:boolean;
 clickCounterObj=clickCounterObj;
 currentSort = { column: 'id', direction: 'asc' };
+
+headers:any[]=[]
+
 
 headersOfLightTable =[
   {
@@ -29,26 +32,21 @@ headersOfLightTable =[
     text: "שיעור שינוי מאומתים"
   },
 
+  {
+    ContainerNgClass: { 'deep-blue': this.isDarkModeActive },
+    graphName: 'lights',
+    sortField: 'email',
+    ContainerClass: 'align-div cell title',
+    text: "% הבדיקות החיוביות"
+  },
 
-
-    {
-      ContainerNgClass: { 'deep-blue': this.isDarkModeActive },
-      graphName: 'lights',
-      sortField: 'email',
-      ContainerClass: 'align-div cell title',
-      text: "% הבדיקות החיוביות"
-    },
-
-
-
-    {
-      ContainerNgClass: { 'deep-blue': this.isDarkModeActive },
-      graphName: 'lights',
-      sortField: 'age',
-      ContainerClass: 'align-div cell title',
+  {
+    ContainerNgClass: { 'deep-blue': this.isDarkModeActive },
+    graphName: 'lights',
+    sortField: 'age',
+    ContainerClass: 'align-div cell title',
       text: "חולים חדשים לכל 10,000 נפש"
     },
-
 
     {
       ContainerNgClass: { 'deep-blue': this.isDarkModeActive },
@@ -58,7 +56,6 @@ headersOfLightTable =[
       text: "ציון וצבע יומי"
     },
 
-
     {
       ContainerNgClass: { 'deep-blue': this.isDarkModeActive },
       graphName: 'lights',
@@ -67,18 +64,17 @@ headersOfLightTable =[
       text: "ציון וצבע יומי"
     },
 
+  ];
 
+  headersOfVaccinationTable = [
 
-]
-headersOfVaccinationTable = [
-
-  {
-    ContainerNgClass: { 'lavender-background': true,'deep-blue':this.isDarkModeActive },
-    graphName: 'Vaccination',
-    sortField: 'address',
-    ContainerClass: 'align-div cell title',
-    text: "ישוב"
-  },
+    {
+      ContainerNgClass: { 'lavender-background': true,'deep-blue':this.isDarkModeActive },
+      graphName: 'Vaccination',
+      sortField: 'address',
+      ContainerClass: 'align-div cell title',
+      text: "ישוב"
+    },
 
     {
       ContainerNgClass:{ 'lavender-background': true, 'deep-blue': this.isDarkModeActive },
@@ -89,8 +85,8 @@ headersOfVaccinationTable = [
     },
 
     {
-        ContainerNgClass: { 'lavender-background': true, 'deep-blue': this.isDarkModeActive },
-        graphName: 'Vaccination',
+      ContainerNgClass: { 'lavender-background': true, 'deep-blue': this.isDarkModeActive },
+      graphName: 'Vaccination',
         sortField: 'email',
         ContainerClass: 'align-div cell title',
         text: "% מתחסנים מנה שנייה"
@@ -122,18 +118,22 @@ headersOfVaccinationTable = [
       },
 
 
-];
-headers=this.graphName==='lights'?this.headersOfLightTable:this.headersOfVaccinationTable;
+    ];
 
+    ngOnInit(): void {
+      console.log(this.graphName);
+      this.headers = this.graphName === 'lights' ? this.headersOfLightTable : this.headersOfVaccinationTable;
+      console.log(this.headers);
+    }
 
-resortTableData(column: string) {
-  if(this.clickCounterObj[column]==2)
-  this.clickCounterObj[column]=0;
-  else
-  this.clickCounterObj[column]+=1
+    resortTableData(column: string) {
+      if(this.clickCounterObj[column]==2)
+      this.clickCounterObj[column]=0;
+      else
+      this.clickCounterObj[column]+=1
 
-  if (column === this.currentSort.column) {
-    this.currentSort.direction = this.currentSort.direction === 'asc' ? 'desc' : 'asc';
+      if (column === this.currentSort.column) {
+        this.currentSort.direction = this.currentSort.direction === 'asc' ? 'desc' : 'asc';
   } else {
     this.currentSort.column = column;
     this.currentSort.direction = 'asc';
