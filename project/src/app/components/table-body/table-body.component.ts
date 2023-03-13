efad3f3038a61461cc1ec31698d22c846c5171b9
lior daniel <liorqm@gmail.com>
2023-03-13 21:56:52 +0200

    rggrgr

--- project/src/app/components/table-body/table-body.component.ts
+++ project/src/app/components/table-body/table-body.component.ts
@@ -11,4 +11,21 @@ export class TableBodyComponent {
 @Input()isDarkModeActive?:boolean;
 @Input() isTable?:boolean;
 
+getNgClassColor(row:any,position:number){
+  const firstObj={
+    green: row.phone < 4.5,
+    yellow: row.phone > 4.5 && row.phone < 6,
+    orange: row.phone > 6 && row.phone < 7.5,
+    red: row.phone > 7.5,
+    color: this.graphName == 'lights',
+    fontWeight400: this.graphName == 'Vaccination'
+  }
+  const secondObj={
+    'color2': this.graphName == 'Vaccination',
+    'lightgreen': row.id < 4.5,
+    'yellow': row.id > 4.6 && row.id < 5.9,
+    'orange': row.id > 6.1 && row.id < 6.8
+}
+return position==2?secondObj:firstObj;
+}
 }
